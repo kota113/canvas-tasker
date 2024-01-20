d44f2001c5d6328b91e204b098c347ab1fa09e4f
Alpaca131 <66709717+Alpaca131@users.noreply.github.com>
2024-01-20 14:43:13 +0900

    use dotenv

--- envs.py
+++ envs.py
@@ -1,5 +1,7 @@
 import os
+import dotenv
 
+dotenv.load_dotenv()
 OAUTH2_CLIENT_ID = os.environ["OAUTH2_CLIENT_ID"]
 OAUTH2_CLIENT_SECRET = os.environ["OAUTH2_CLIENT_SECRET"]
 SESSION_SECRET = os.environ["SESSION_SECRET"]
